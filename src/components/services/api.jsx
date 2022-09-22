c8a4e03bc301e55adcbb983d4d8110cf9619d257
Ivan Shvayka <shvaykaofficial@gmail.com>
2022-09-22 11:35:14 +0300

    refactor code

---
+++ src/components/services/api.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+axios.defaults.params = {
+  key: '29840088-005b83d6ddf8fc8359634331b',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
+
+export const fetchImages = async (query, page) => {
+  const { data } = await axios.get(`?q=${query}&page=${page}`);
+  return data;
+};
+
+// const response = await axios.get(
+//   `https://pixabay.com/api/?key=27697316-9cc45c303ea5cb91afbaa3e72&q=${query}&image_type=photo&per_page=12`
+// );
